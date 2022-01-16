d4205e2d8556d3f4eba9fcce663d02fe9fdb58c8
akhilankathi <akhilankathi@gmail.com>
2022-01-16 20:18:29 +0530

    changes image gallery app added

--- image_search_app/src/App.js
+++ image_search_app/src/App.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { createRef, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 import axios from 'axios';
-import  Image from './Image'
+import  Image from './Image';
+// import { forwardRef ,useEffect } from 'react';
+
 
 
 const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
 function App() {
   const [data,setData] = useState([]);
   const [search , setSearch] = useState("");
-
+  const inputRef = React.createRef()
+ useEffect(()=>{
+  inputRef.current.focus()
+ },[])
   const handlechange = (e)=>{
     setSearch(e.target.value);
   }
@@ -30,14 +35,16 @@ function App() {
     );
 })
 }
+
   return (
     <div>
       <center>
+        <h2 style={{"background-color" : "grey" , "height":"60px" ,"padding-top" : "20px" , "fontFamily" :"sans-serif" , "color": "goldenrod"}}>IMGAE GALLERY APP </h2>
         <form>
-          <input type="text"  value = {search} onChange={handlechange}/> <br/> <br/>
-          <button type="button" name="search" onClick={(e)=>getdata(e)}>Search</button>
-        </form>
-        {data.length >= 0 ? <Image data = {data} /> : <h1>NO DATA LOADED</h1>}
+          <input ref={inputRef} style={{"width": "200px" , "height":"30px"}}  type="text"  value = {search} onChange={handlechange}/> <br/> <br/>
+          <button style={{"width": "60px" , "height":"30px"}} type="button" name="search" onClick={(e)=>getdata(e)}>Search</button>
+        </form> <br />
+        {data.length >= 1 ? <Image data = {data} /> : <h1>NO DATA LOADED</h1>}
       </center>
     </div>
   );
--- image_search_app/src/Image.jsx
+++ image_search_app/src/Image.jsx
@@ -1,13 +1,13 @@
 const Image =({data})=>{
-return <div>
-    <div >
-        {data.map((image) =><div key={image.id}>
+return <span >
+        {data.map((image) =><span key={image.id}>
         <span >
         <img src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`} 
        height="200" width="250" alt={image.title}/>
-        </span>
-        </div>)}
-        </div>
-</div>
+        </span> </span>
+        )} </span>
+       
+        
+
 }
 export default Image
